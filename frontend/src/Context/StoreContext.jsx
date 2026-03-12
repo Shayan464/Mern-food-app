e91b19da44e56998726b2b65fbd74b88011f7e45
shayan <shayan8839@gmail.com>
2026-03-13 00:57:13 +0530

    'frontend url updated'

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = 'http://localhost:4000';
+  const url = 'https://mern-food-app-backend-3ops.onrender.com';
   const [token, setToken] = useState('');
   const [food_list, setFood_list] = useState([]);
 
